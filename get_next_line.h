722be762a3403122b4ce3e825b202306c9c1f294
lulm <lionelulm67@gmail.com>
2024-02-21 16:29:52 +0100

    added includes

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,18 @@
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:03:34 by lionelulm         #+#    #+#             */
-/*   Updated: 2024/02/21 16:01:36 by lionelulm        ###   ########.fr       */
+/*   Updated: 2024/02/21 16:25:08 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
