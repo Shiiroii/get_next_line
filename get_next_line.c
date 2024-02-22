c3633fc84a5d01c64a5774e948d618c7849d929e
lulm <lionelulm67@gmail.com>
2024-02-22 12:19:39 +0100

    updated get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:02:27 by lionelulm         #+#    #+#             */
-/*   Updated: 2024/02/22 11:42:53 by lionelulm        ###   ########.fr       */
+/*   Updated: 2024/02/22 12:18:50 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,30 @@ char	*freeline(char *buffer, char *c)
 	return (yes);
 }
 
-char	*read_line(int fd, char *str)
+// char	*read_line(int fd, char *str)
+// {
+// 	char	*buffer;
+// 	int		i;
+
+// 	i = 1;
+// 	if (str == NULL)
+// 		str = ft_calloc(1, 1);
+// 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+// 	if (buffer == NULL)
+// 		return (free(str), NULL);
+// 	while (i > 0 && ft_strchr(buffer, '\n') == NULL)
+// 	{
+// 		i = read(fd, buffer, BUFFER_SIZE);
+// 		if (i < 0)
+// 			return (free(buffer), NULL);
+// 		buffer[i] = 0;
+// 		str = freeline(str, buffer);
+// 	}
+// 	free(buffer);
+// 	return (str);
+// }
+
+char *read_line(int fd, char *str)
 {
 	char	*buffer;
 	int		i;
@@ -32,12 +55,9 @@ char	*read_line(int fd, char *str)
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (buffer == NULL)
 		return (free(str), NULL);
-	while (i > 0 && ft_strchr(buffer, '\n') == NULL)
+	while ((i = read(fd, buffer, BUFFER_SIZE)) > 0 || ft_strchr(buffer, '\n') == NULL)
 	{
-		i = read(fd, buffer, BUFFER_SIZE);
-		if (i < 0)
-			return (free(buffer), NULL);
-		buffer[i] = 0;
+		buffer[i] = '\0';
 		str = freeline(str, buffer);
 	}
 	free(buffer);
--- main.c
+++ main.c
@@ -6,18 +6,40 @@
 /*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:18:45 by lionelulm         #+#    #+#             */
-/*   Updated: 2024/02/22 11:32:07 by lionelulm        ###   ########.fr       */
+/*   Updated: 2024/02/22 12:08:11 by lionelulm        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	main()
-{
-	int fd;
+// int	main()
+// {
+// 	int fd;
 
-	fd = open("test1.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
+// 	fd = open("test1.txt", O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	close(fd);
+// 	return (0);
+// }
+
+int main(int argc, char **argv) {
+	if (argc != 2)
+	{
+		printf("Usage: %s <filename>\n", argv[0]);
+		return (1);
+	}
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	char	*line;
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
 	close(fd);
 	return (0);
 }
