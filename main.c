4cf8087d447d4a01a81ea57abdb1b94340a0b9bb
Shiroii || Lio <139677855+Shiiroii@users.noreply.github.com>
2024-05-03 09:15:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,33 +12,6 @@
 
 #include "get_next_line.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	int		fd;
-// 	char	*line;
-
-// 	if (argc != 2)
-// 	{
-// 		printf("Usage: %s <filename>\n", argv[0]);
-// 		return (1);
-// 	}
-// 	fd = open(argv[1], O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		perror("Error");
-// 		return (1);
-// 	}
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("%s\n", line);
-// 		free(line);
-// 	}
-// 	if (line == NULL)
-// 		perror("Error");
-// 	close(fd);
-// 	return (0);
-// }
-
 int	main(int argc, char **argv)
 {
 	int		fd;
@@ -46,7 +19,7 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("Usage: %s <filename>\n", argv[0]);
+		printf("Usage: %s <file>\n", argv[0]);
 		return (1);
 	}
 	fd = open(argv[1], O_RDONLY);
